9e3737c3ca4ad08d00725cd3644f610e2b029bc0
iAnoeloeby <anoeloebynda06@gmail.com>
2026-06-12 22:03:38 +0700

    chore: add shadcn ui for reusable components

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,21 +1,27 @@
-import { defineConfig } from "vite";
-import { resolve } from "path";
-import { fileURLToPath } from "url";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import path, { resolve } from "path";
+import { fileURLToPath } from "url";
 
 const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
 export default defineConfig({
-  base: "./",
-  publicDir: "public",
-  plugins: [tailwindcss()],
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        newtab: resolve(__dirname, "newtab.html"),
-      },
+    base: "./",
+    publicDir: "public",
+    plugins: [react(), tailwindcss()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
+    build: {
+        outDir: "dist",
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                newtab: resolve(__dirname, "newtab.html"),
+            },
+        },
+    },
 });
