690281306344c1054f090a8e36b1afa6a2bb65b3
iAnoeloeby <anoeloebynda06@gmail.com>
2026-06-14 15:52:29 +0700

    refactor: update item & popover components

--- src/components/ui/Item.jsx
+++ src/components/ui/Item.jsx
@@ -1,17 +1,78 @@
-import React from "react";
-import { cn } from "@/lib/utils";
+import React, { forwardRef } from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { cva } from "class-variance-authority";
 
-export default function Item({ className = "", children }) {
+import { cn } from "@/lib/utils";
+import { PlusIcon } from "@phosphor-icons/react";
+
+const itemVariants = cva(
+    "group/item relative inline-flex shrink-0 p-2 items-center justify-center rounded-lg border border-transparent bg-clip-padding transition-all outline-none select-none focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/30 active:not-aria-[haspopup]:translate-y-px disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+    {
+        variants: {
+            variant: {
+                default: "bg-surface text-primary-foreground",
+                ghost: "border-border bg-background hover:bg-muted hover:text-foreground aria-expanded:bg-muted aria-expanded:text-foreground dark:bg-transparent dark:hover:bg-input/30",
+                secondary:
+                    "bg-secondary text-secondary-foreground hover:bg-[color-mix(in_oklch,var(--secondary),var(--foreground)_5%)] aria-expanded:bg-secondary aria-expanded:text-secondary-foreground",
+            },
+            size: {
+                default: "size-12 [&_svg:not([class*='size-'])]:size-6",
+                xs: "size-8 [&_svg:not([class*='size-'])]:size-4",
+                sm: "size-10 [&_svg:not([class*='size-'])]:size-5",
+                lg: "size-14 [&_svg:not([class*='size-'])]:size-8",
+            },
+        },
+        defaultVariants: {
+            variant: "default",
+            size: "default",
+        },
+    },
+);
+
+/**
+ * @typedef {Object} ItemProps
+ * @property {boolean} [asChild]
+ * @property {React.MouseEventHandler<HTMLDivElement>} [onClick]
+ * @property {React.ReactNode} children
+ * @property {string} [className]
+ * @property {"default" | "ghost" | "secondary"} [variant]
+ * @property {"xs"|"sm"|"default"|"lg"} [size]
+ */
+
+/** @type {React.ForwardRefRenderFunction<HTMLDivElement, ItemProps>} */
+const Item = forwardRef(
+    (
+        {
+            asChild = false,
+            className = "",
+            variant = "default",
+            size = "default",
+            ...props
+        },
+        ref,
+    ) => {
+        return (
+            <div
+                ref={ref}
+                className={cn(itemVariants({ variant, size }), className)}
+                {...props}
+            />
+        );
+    },
+);
+
+Item.displayName = "Item";
+
+function ItemAdd({ actions }) {
     return (
-        <div
-            className={cn(
-                "relative inline-flex size-12 shrink-0 items-center justify-center rounded-lg border border-border bg-surface p-2 dark:border-white/20 z-10",
-                className,
-            )}
+        <Item
+            onClick={actions}
+            variant="secondary"
+            className="relative z-2 bg-surface/50 group-hover:border-border"
         >
-            {children}
-        </div>
+            <PlusIcon size={20} weight="bold" aria-hidden="true" />
+        </Item>
     );
 }
 
-Item;
+export { Item, ItemAdd };
