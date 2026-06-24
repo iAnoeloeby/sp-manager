0b8d7a49381a49079e2877e1f424b763a0ba5e4a
iAnoeloeby <anoeloebynda06@gmail.com>
2026-06-25 02:46:41 +0700

    refactor: update shortcut widget implementation

--- src/features/shortcuts/layout/ShortcutWidget.jsx
+++ src/features/shortcuts/layout/ShortcutWidget.jsx
@@ -1,12 +1,5 @@
 import React from "react";
-import ShortcutCard, {
-    ShortcutCardSkeleton,
-} from "@/features/shortcuts/components/ShortcutCard";
-import { useShortcuts } from "@/features/shortcuts/hooks/useShortcuts";
-
-/**
- * @typedef {import("../components/ShortcutContextMenu").Shortcut} Shortcut
- */
+import ShortcutCard from "@/features/shortcuts/components/ShortcutCard";
 
 /**
  * @typedef {Object} ShortcutWidgetProps
@@ -14,30 +7,9 @@ import { useShortcuts } from "@/features/shortcuts/hooks/useShortcuts";
  * @property {(s: Shortcut) => void} onAddShortcut
  * @property {(id: string, patch: Partial<Shortcut>) => void} onUpdateShortcut
  * @property {(id: string) => void} onDeleteShortcut
- */
-
-/**
- * Top-level widget yang menggabungkan grid + animasi hover + popover + context menu.
- *
- * Struktur:
- *   <>
- *     {shortcuts.length ? <ShortcutGrid /> : <EmptyState />}
- *     <ShortcutForm open={isShortcutFormOpen} ... />  <-- selalu di-mount
- *   </>
- *
- * `ShortcutForm` HARUS di-render di return utama (bukan di dalam `action`),
- * supaya saat grid berisi item, klik tile "+" / context-menu Edit bisa
- * membuka dialog. Kalau cuma di dalam `action` EmptyState, dialog baru
- * muncul saat grid kosong.
  *
  * @param {ShortcutWidgetProps} props
  */
 export default function ShortcutWidget() {
-    const { loading } = useShortcuts();
-
-    if (loading) {
-        return <ShortcutCardSkeleton />;
-    }
-
     return <ShortcutCard />;
 }
